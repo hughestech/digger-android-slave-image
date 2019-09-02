a648858c433f13a6cf85c1b63ad4eabb957186d8
magick93 <kurrent93@gmail.com>
2019-09-02 20:38:19 +1200

    use gpg

--- scripts/rubyoncentos.sh
+++ scripts/rubyoncentos.sh
@@ -6,6 +6,8 @@ echo "Starting Installation ruby 2.5..."
 yum install -y gcc-c++ patch readline readline-devel zlib zlib-devel libyaml-devel libffi-devel openssl-devel make bzip2 autoconf automake libtool bison iconv-devel ruby-devel libxml2 libxml2-devel libxslt libxslt-devel git
 
 # Install Ruby from rvm
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+
 \curl -sSL https://get.rvm.io | bash -s stable --ruby
 
 #Enable rvm in current shell
